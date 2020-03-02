93328096c040ce061f1032efd70b53f25cf50816
Brittany Russell <sincerelybr@gmail.com>
2020-03-02 01:12:39 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,41 @@
+# class Dog
+#     def initialize(name)
+#     @name = name
+#   end
+
+#   def name=(name)
+#     @name = name
+#   end
+
+#   def name
+#     @name
+#   end
+  
+#   def initialize(breed)
+#     @breed = breed
+#   end
+
+#   def breed=(breed)
+#     @breed = "Mutt"
+#   end
+
+#   def breed 
+#     @breed
+#   end
+# end
+
+
+
+class Dog
+  def initialize(breed)
+    @breed = breed
+  end
+
+  # def breed=(breed)
+  #   @breed = breed
+  # end
+
+  # def breed
+  #   @breed
+  # end
+end
